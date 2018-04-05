2eae4504a230b15f3042cc0355d9b4f5b49249af
Kanyi-PC <mbugua.kevinkanyi@gmail.com>
2018-04-05 18:12:57 +0300

    added second cache

---
+++ EhCaching/src/main/java/co/ke/bigfootke/app/SecondCache.java
@@ -0,0 +1,28 @@
+package co.ke.bigfootke.app;
+
+public class SecondCache {
+ private int cacheSize;
+ private int flush;
+ 
+ public SecondCache() {
+	 
+ }
+
+public int getCacheSize() {
+	return cacheSize;
+}
+
+public void setCacheSize(int cacheSize) {
+	this.cacheSize = cacheSize;
+}
+
+public int getFlush() {
+	return flush;
+}
+
+public void setFlush(int flush) {
+	this.flush = flush;
+}
+ 
+ 
+}
