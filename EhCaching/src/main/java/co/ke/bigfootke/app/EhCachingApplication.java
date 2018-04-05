351e06424d5cc5751c3bafa8f8e191fbb80bc30e
Kanyi-PC <mbugua.kevinkanyi@gmail.com>
2018-04-05 17:58:32 +0300

    Second commit

--- EhCaching/src/main/java/co/ke/bigfootke/app/EhCachingApplication.java
+++ EhCaching/src/main/java/co/ke/bigfootke/app/EhCachingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EhCachingApplication {
-
+//Main app
 	public static void main(String[] args) {
 		SpringApplication.run(EhCachingApplication.class, args);
 	}
